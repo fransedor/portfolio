8e3cfe25d1943cce3aee01acc9cca0814265b759
Fransedo Ravelino <fransedo.ravelino@bluebirdgroup.com>
2024-03-25 15:23:21 +0700

    added experience content

--- src/json/content.ts
+++ src/json/content.ts
@@ -20,6 +20,47 @@ export const CONTENTS: Record<"experience" | "projects", ContentType[]> = {
         "Assisted other frontend engineers when implementing new libraries, such as React Query, NextJS, and Verdaccio and ensuring efficient implementation through code reviews.",
       ],
     },
+    {
+      title: "Frontend Engineering Tutor",
+      company: "MySkill",
+      description:
+        "MySkill is a upskilling & career preparation startup for aspiring professionals, helping individuals and companies to achieve their new heights by crafting their skills.",
+      points: [
+        "Provide a learning curriculum for Frontend debugging, along with all the learning materials needed",
+        "Provide the learning materials for introduction to HTML course",
+      ],
+    },
+    {
+      title: "Frontend Engineer",
+      company: "Society of Renewable Energy (SRE)",
+      description:
+        "An NGO accelerating Indonesia energy transition through youth empowerment and multi-institutional collaboration",
+      points: [
+        "Maintain and update landing page using NextJS, TypeScript and Firebase, allowing 1000 users to participate in SRE multi-institutional projects.",
+        "Developed an E-commerce platform for SRE partners using NextJS and Typescript.",
+      ],
+    },
+    {
+      title: "Frontend Engineer",
+      company: "Arvis",
+      description:
+        "Arvis is a Software Development company, providing solutions for real problems.",
+      points: [
+        "Developed an MVP for an agricultural startup with NextJS, Redux, and TypeScript, leading the startup to a Seed round funding.",
+        "Developed a stock management app using React, Express, and PostgreSQL for a client’s yearly stock take.",
+      ],
+    },
+    {
+      title: "Frontend Engineer",
+      company: "Loocale.id",
+      description:
+        "Loocale is a tourism startup aiming to bridge the gap between tourism provider and tourists",
+      points: [
+        "Developed an MVP for loocale using NextJS, Express, and MySQL, leading to a funding from a university incubation program",
+      ],
+    },
   ],
-  projects: [],
+  projects: [
+		
+	],
 };
