f527602720166b9949107f96d2b4948bc566b91b
fransedor <fransedoravelino@gmail.com>
2024-04-07 20:42:34 +0700

    added contacts

--- src/components/MenuPage.tsx
+++ src/components/MenuPage.tsx
@@ -39,9 +39,20 @@ const MenuPage = () => {
             <div className="border-x-4 border-white mt-auto w-full bg-transparent"></div>
           </div>
         </div>
-        <button className="sm:h-full cursor-pointer text-white text-3xl p-4 w-screen sm:bg-[length:300%_100%] sm:text-[80px] flex items-center px-20 transition-all duration-500 ease-in bg-size-200  bg-left hover:bg-right bg-gradient-to-r from-black from-30% via-green-800 to-green-500">
-          CONTACT
-        </button>
+        <div className="sm:h-full gap-6 text-black p-4 w-screen sm:bg-[length:300%_100%] sm:text-[80px] flex justify-center items-center  transition-all duration-500 ease-in bg-size-200  bg-left hover:bg-right bg-gradient-to-r from-black from-30% via-green-800 to-green-500">
+          <a href="https://github.com/fransedor" target="_blank" className="flex gap-2 sm:gap-4 py-1 sm:py-3 px-2 sm:px-6 rounded-full bg-white text-sm sm:text-xl items-center">
+						<img src="/icons/github.svg" alt="github" className="w-5 h-5" />
+						<span className="font-bold ">Github</span>
+					</a>
+          <a href="https://github.com/fransedor" target="_blank" className="flex gap-2 sm:gap-4 py-1 sm:py-3 px-2 sm:px-6 rounded-full bg-white text-sm sm:text-xl items-center">
+						<img src="/icons/github.svg" alt="github" className="w-5 h-5" />
+						<span className="font-bold ">LinkedIn</span>
+					</a>
+          <a href="mailto:fransedoravelino@gmail.com" target="_blank" className="flex gap-2 sm:gap-4 py-1 sm:py-3 px-2 sm:px-6 rounded-full bg-white text-sm sm:text-xl items-center">
+						<img src="/icons/github.svg" alt="github" className="w-5 h-5" />
+						<span className="font-bold ">Email</span>
+					</a>
+        </div>
       </div>
       <Modal
         open={modalType !== undefined}
